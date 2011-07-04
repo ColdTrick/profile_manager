2d7bb7cd96f3e38b7f52b4b7f54a8ad87d9965d9
jdalsem <jdalsem@coldtrick.com>
2011-07-04 14:19:51 +0200

    betere error message

--- test.php
+++ test.php
@@ -1,10 +1,9 @@
 <?php
 	function test(){
 	
-		echo "this is just a dummy function";
+		echo "this is just a dummy function!";
 		
-		while(1==1)
-		{
+		while(1==1){
 			if($i = 1){
 				echo 'ja he';
 			} 
