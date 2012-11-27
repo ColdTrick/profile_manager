ca2f5b27214dc4e583a86f6d0ed35e2e2c5b7ab9
Jeroen Dalsem <jdalsem@coldtrick.com>
2012-11-27 13:49:15 +0100

    accessability improvements

--- views/default/forms/register.php
+++ views/default/forms/register.php
@@ -22,8 +22,8 @@ if($accept_terms = elgg_get_plugin_setting("registration_terms", "profile_manage
 	$link_end = "</a>";
 	
 	$terms = "<div class='mandatory'>";
-	$terms .= "<input type='checkbox' name='accept_terms' value='yes' /> ";
-	$terms .= "<label>" . elgg_echo("profile_manager:registration:accept_terms", array($link_begin, $link_end)) . "</label>";
+	$terms .= "<input id='register-accept_terms' type='checkbox' name='accept_terms' value='yes' /> ";
+	$terms .= "<label for='register-accept_terms'>" . elgg_echo("profile_manager:registration:accept_terms", array($link_begin, $link_end)) . "</label>";
 	$terms .= "</div>";
 }
 
@@ -32,45 +32,51 @@ echo "<div id='profile_manager_register_left'>";
 
 <fieldset>
 	<div class="mtm mandatory">
-		<label><?php echo elgg_echo('name'); ?></label><br />
+		<label for='register-name'><?php echo elgg_echo('name'); ?></label><br />
 		<?php
 		echo elgg_view('input/text', array(
+			'id' => 'register-name',
 			'name' => 'name',
 			'value' => $name,
+			'class' => 'elgg-autofocus'
 		));
 		?>
 	</div>
 	<div class="mandatory">
-		<label><?php echo elgg_echo('email'); ?></label><br />
+		<label for='register-email'><?php echo elgg_echo('email'); ?></label><br />
 		<?php
 		echo elgg_view('input/text', array(
+			'id' => 'register-email',
 			'name' => 'email',
 			'value' => $email,
 		));
 		?>
 	</div>
 	<div class="mandatory">
-		<label><?php echo elgg_echo('username'); ?></label><br />
+		<label for='register-username'><?php echo elgg_echo('username'); ?></label><br />
 		<?php
 		echo elgg_view('input/text', array(
+			'id' => 'register-username',
 			'name' => 'username',
 			'value' => $username,
 		));
 		?>
 	</div>
 	<div class="mandatory">
-		<label><?php echo elgg_echo('password'); ?></label><br />
+		<label for='register-password'><?php echo elgg_echo('password'); ?></label><br />
 		<?php
 		echo elgg_view('input/password', array(
+			'id' => 'register-password',
 			'name' => 'password',
 			'value' => $password,
 		));
 		?>
 	</div>
 	<div class="mandatory">
-		<label><?php echo elgg_echo('passwordagain'); ?></label><br />
+		<label for='register-password2'><?php echo elgg_echo('passwordagain'); ?></label><br />
 		<?php
 		echo elgg_view('input/password', array(
+			'id' => 'register-password2',
 			'name' => 'password2',
 			'value' => $password2,
 		));
@@ -98,16 +104,12 @@ echo "<div class='elgg-foot'>";
 echo elgg_view('input/hidden', array('name' => 'friend_guid', 'value' => $vars['friend_guid']));
 echo elgg_view('input/hidden', array('name' => 'invitecode', 'value' => $vars['invitecode']));
 echo elgg_view('input/submit', array('name' => 'submit', 'value' => elgg_echo('register')));
-echo "<div>" . elgg_echo("profile_manager:register:mandatory") . "</div>";
+echo "<div class='elgg-subtext mtm'>" . elgg_echo("profile_manager:register:mandatory") . "</div>";
 echo "</div>";
 
 echo elgg_view("profile_manager/register/js");
 ?>
-<script type="text/javascript">
-	$(function() {
-		$('input[name=name]').focus();
-	});
-</script>
+
 <style type="text/css">
 	.elgg-form-account {
 		max-width: 100%;
