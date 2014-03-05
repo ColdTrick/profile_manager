48537935b2a559f233626080878106ebb2451ebb
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-03-05 11:46:28 +0100

    fixed #51: tags get merged during save if field is non-editable

--- views/default/input/non_editable.php
+++ views/default/input/non_editable.php
@@ -9,7 +9,12 @@
 * @copyright Coldtrick IT Solutions 2009
 * @link http://www.coldtrick.com/
 */
-	
+
+if (is_array($vars["value"])) {
+	// probably tags, so change to text (fixes #51)
+	$vars["value"] = implode(", ", $vars["value"]);
+}
+
 echo elgg_view("input/hidden", $vars);
 	
 echo "<div>";
