888665892f73b8738aaff1582ad647a884c8164d
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-06-02 13:17:54 +0200

    changed: multiselect will show 'x selected' when more than 1 selected

--- views/default/js/profile_manager/multiselect.js
+++ views/default/js/profile_manager/multiselect.js
@@ -7,7 +7,7 @@ define(function(require) {
 	elgg.multiselect.init = function() {
 		$('.profile-manager-multiselect').multiselect({
 			header: false,
-			selectedList: 4,
+			selectedList: 1,
 			noneSelectedText: elgg.echo('profile_manager:input:multi_select:empty_text'),
 			selectedText: elgg.echo('profile_manager:input:multi_select:selected_text')
 		});
