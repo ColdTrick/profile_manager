d3ee78699846410835e94ab359f89baa295316d3
Jeroen Dalsem <jdalsem@coldtrick.com>
2017-09-11 12:03:17 +0200

    chore: small function refactor

--- classes/ColdTrick/ProfileManager/Groups.php
+++ classes/ColdTrick/ProfileManager/Groups.php
@@ -56,50 +56,55 @@ class Groups {
 	 * @return void
 	 */
 	public static function groupsEdit($hook_name, $entity_type, $return_value, $params) {
-		$guid = get_input("group_guid");
-		if (!empty($guid) || !elgg_is_admin_logged_in()) {
-	
-			$group = get_entity($guid);
-	
-			if (elgg_instanceof($group, "group")) {
-				$name_input = get_input("name", false);
-				$description_input = get_input("description", false);
+		
+		if (elgg_is_admin_logged_in()) {
+			// admins can always edit
+			return;
+		}
+		
+		$guid = (int) get_input('group_guid');
+		$group = get_entity($guid);
+		if (!$group instanceof \ElggGroup) {
+			return;
+		}
+		
+		$name_input = get_input("name", false);
+			
+		if (($name_input !== false) && ($name_input !== $group->name)) {
+			$limit = elgg_get_plugin_setting("group_limit_name", "profile_manager");
+
+			if (!empty($limit) || ($limit == "0")) {
+				$limit = (int) $limit;
+				$count = (int) $group->getPrivateSetting("profile_manager_name_edit_count");
 					
-				if (($name_input !== false) && ($name_input !== $group->name)) {
-					$limit = elgg_get_plugin_setting("group_limit_name", "profile_manager");
-	
-					if (!empty($limit) || ($limit == "0")) {
-						$limit = (int) $limit;
-						$count = (int) $group->getPrivateSetting("profile_manager_name_edit_count");
-							
-						if ($count < $limit) {
-							// register function to increment count on succesful edit
-							elgg_register_event_handler("update", "group", "\ColdTrick\ProfileManager\Groups::nameIncrement");
-						} else {
-							// group name needs special treatment
-							$name = htmlspecialchars_decode($group->name, ENT_QUOTES);
-	
-							// cannot be changed, so reset to current value
-							set_input("name", $name);
-						}
-					}
+				if ($count < $limit) {
+					// register function to increment count on succesful edit
+					elgg_register_event_handler("update", "group", "\ColdTrick\ProfileManager\Groups::nameIncrement");
+				} else {
+					// group name needs special treatment
+					$name = htmlspecialchars_decode($group->name, ENT_QUOTES);
+
+					// cannot be changed, so reset to current value
+					set_input("name", $name);
 				}
+			}
+		}
+			
+		
+		$description_input = get_input("description", false);
+		if (($description_input !== false) && ($description_input !== $group->description)) {
+			$limit = elgg_get_plugin_setting("group_limit_description", "profile_manager");
+
+			if (!empty($limit) || ($limit == "0")) {
+				$limit = (int) $limit;
+				$count = (int) $group->getPrivateSetting("profile_manager_description_edit_count");
 					
-				if (($description_input !== false) && ($description_input !== $group->description)) {
-					$limit = elgg_get_plugin_setting("group_limit_description", "profile_manager");
-	
-					if (!empty($limit) || ($limit == "0")) {
-						$limit = (int) $limit;
-						$count = (int) $group->getPrivateSetting("profile_manager_description_edit_count");
-							
-						if ($count < $limit) {
-							// register function to increment count on succesful edit
-							elgg_register_event_handler("update", "group", "\ColdTrick\ProfileManager\Groups::descriptionIncrement");
-						} else {
-							// cannot be changed, so reset to current value
-							set_input("description", $group->description);
-						}
-					}
+				if ($count < $limit) {
+					// register function to increment count on succesful edit
+					elgg_register_event_handler("update", "group", "\ColdTrick\ProfileManager\Groups::descriptionIncrement");
+				} else {
+					// cannot be changed, so reset to current value
+					set_input("description", $group->description);
 				}
 			}
 		}
