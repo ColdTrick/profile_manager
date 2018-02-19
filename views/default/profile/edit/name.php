175d3348ecc0d0f189784c67559247e2a22f1233
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-02-19 11:19:11 +0100

    chore: updated to elgg_view_field

--- views/default/profile/edit/name.php
+++ views/default/profile/edit/name.php
@@ -1,6 +1,9 @@
 <?php
 
-$content = elgg_format_element('label', [], elgg_echo('user:name:label'));
-$content .= elgg_view('input/text', ['name' => 'name', 'value' => $vars['entity']->name]);
-
-echo elgg_format_element('div', [], $content);
+echo elgg_view_field([
+	'#type' => 'text',
+	'name' => 'name',
+	'value' => $vars['entity']->name,
+	'#label' => elgg_echo('user:name:label'),
+	'maxlength' => 50, // hard coded in /actions/profile/edit
+]);
