994ac8d7b008c8a52306bc6a9a914090a58c8e92
Dennis Ploeger <develop@dieploegers.de>
2018-07-11 22:09:15 +0200

    Walking over the found multiselects rather than chaining the calls. closes 114

--- views/default/js/profile_manager/multiselect.js
+++ views/default/js/profile_manager/multiselect.js
@@ -5,12 +5,17 @@ define(function(require) {
 	elgg.provide('elgg.multiselect');
 	
 	elgg.multiselect.init = function() {
-		$('.profile-manager-multiselect').multiselect({
-			header: false,
-			appendTo: "body",
-			selectedList: 1,
-			noneSelectedText: elgg.echo('profile_manager:input:multi_select:empty_text'),
-			selectedText: elgg.echo('profile_manager:input:multi_select:selected_text')
-		}).multiselect('getButton').find('.ui-icon').addClass('elgg-icon elgg-icon-caret-down fa fa-caret-down');
+		$('.profile-manager-multiselect').toArray().forEach(
+			function (select) {
+				$(select).multiselect({
+					header: false,
+					appendTo: "body",
+					selectedList: 1,
+					noneSelectedText: elgg.echo('profile_manager:input:multi_select:empty_text'),
+					selectedText: elgg.echo('profile_manager:input:multi_select:selected_text')
+				});
+				$(select).multiselect('getButton').find('.ui-icon').addClass('float-alt link elgg-icon elgg-icon-caret-down fa fa-caret-down prs');
+			}
+		)
 	};
 });
