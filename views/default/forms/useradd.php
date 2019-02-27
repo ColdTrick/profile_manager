0b801761c552574bb86d53e01f2c15f296769da0
Jerome Bakker <jeabakker@coldtrick.com>
2019-02-27 17:07:18 +0100

    changed: updated useradd form with core defaults

--- views/default/forms/useradd.php
+++ views/default/forms/useradd.php
@@ -1,89 +1,105 @@
 <?php
+
 /**
  * Elgg add user form.
  *
  * @package Elgg
  * @subpackage Core
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
- * @author Curverider Ltd
- * @copyright Curverider Ltd 2008-2009
- * @link http://elgg.org/
+ *
  */
-
-$name = $username = $email = $password = $password2 = $admin = '';
+elgg_require_js('forms/useradd');
 
 if (elgg_is_sticky_form('useradd')) {
 	$values = elgg_get_sticky_values('useradd');
-	extract($values);
 	elgg_clear_sticky_form('useradd');
-	if (is_array($admin)) {
-		$admin = $admin[0];
-	}
+} else {
+	$values = array();
 }
+
+$password = $password2 = '';
+$name = elgg_extract('name', $values);
+$username = elgg_extract('username', $values);
+$email = elgg_extract('email', $values);
+$admin = elgg_extract('admin', $values);
+$autogen_password = elgg_extract('autogen_password', $values);
 
 $admin_option = false;
-if ((elgg_get_logged_in_user_entity()->isAdmin()) && ($vars['show_admin'])) {
+if (elgg_is_admin_logged_in() && elgg_extract('show_admin', $vars)) {
 	$admin_option = true;
 }
-?>
-<div>
-	<label><?php echo elgg_echo('name');?></label><br />
-	<?php
-	echo elgg_view('input/text', array(
-		'name' => 'name',
-		'value' => $name,
-	));
-	?>
-</div>
-<div>
-	<label><?php echo elgg_echo('username'); ?></label><br />
-	<?php
-	echo elgg_view('input/text', array(
-		'name' => 'username',
-		'value' => $username,
-	));
-	?>
-</div>
-<div>
-	<label><?php echo elgg_echo('email'); ?></label><br />
-	<?php
-	echo elgg_view('input/text', array(
-		'name' => 'email',
-		'value' => $email,
-	));
-	?>
-</div>
-<div>
-	<label><?php echo elgg_echo('password'); ?></label><br />
-	<?php
-	echo elgg_view('input/password', array(
-		'name' => 'password',
-		'value' => $password,
-	));
-	?>
-</div>
-<div>
-	<label><?php echo elgg_echo('passwordagain'); ?></label><br />
-	<?php
-	echo elgg_view('input/password', array(
-		'name' => 'password2',
-		'value' => $password2,
-	));
-	?>
-</div>
 
-<?php
+echo elgg_view_field([
+	'#type' => 'text',
+	'name' => 'name',
+	'value' => $name,
+	'#label' => elgg_echo('name'),
+	'required' => true,
+]);
+
+echo elgg_view_field([
+	'#type' => 'text',
+	'name' => 'username',
+	'value' => $username,
+	'#label' => elgg_echo('username'),
+	'required' => true,
+]);
+
+echo elgg_view_field([
+	'#type' => 'email',
+	'name' => 'email',
+	'value' => $email,
+	'#label' => elgg_echo('email'),
+	'required' => true,
+]);
+
+echo elgg_view_field(array(
+	'#type' => 'checkbox',
+	'name' => 'autogen_password',
+	'value' => 1,
+	'default' => false,
+	'label' => elgg_echo('autogen_password_option'),
+	'checked' => (bool) $autogen_password,
+));
+
+echo elgg_view_field([
+	'#type' => 'password',
+	'name' => 'password',
+	'value' => $password,
+	'#label' => elgg_echo('password'),
+	'required' => true,
+]);
+
+echo elgg_view_field([
+	'#type' => 'password',
+	'name' => 'password2',
+	'value' => $password2,
+	'#label' => elgg_echo('passwordagain'),
+	'required' => true,
+]);
+
 if ($admin_option) {
-	echo "<div>";
-	echo elgg_view('input/checkboxes', array(
-		'name' => "admin",
-		'options' => array(elgg_echo('admin_option') => 1),
-		'value' => $admin,
+	echo elgg_view_field(array(
+		'#type' => 'checkbox',
+		'name' => 'admin',
+		'value' => 1,
+		'default' => false,
+		'label' => elgg_echo('admin_option'),
+		'checked' => $admin,
 	));
-	echo "</div>";
 	
-	echo "<div>" . elgg_view('input/checkboxes', array('name' => "notify", 'options' => array(elgg_echo('profile_manager:admin:adduser:notify') => 1))) . "</div>";
-	echo "<div>" . elgg_view('input/checkboxes', array('name' => "use_default_access", 'value' => elgg_echo('profile_manager:admin:adduser:use_default_access'), 'options' => array(elgg_echo('profile_manager:admin:adduser:use_default_access') => 1))) . "</div>";
+	echo elgg_view_field(array(
+		'#type' => 'checkbox',
+		'name' => 'notify',
+		'value' => 1,
+		'label' => elgg_echo('profile_manager:admin:adduser:notify'),
+	));
+	
+	echo elgg_view_field(array(
+		'#type' => 'checkbox',
+		'name' => 'use_default_access',
+		'value' => 1,
+		'label' => elgg_echo('profile_manager:admin:adduser:use_default_access'),
+	));
 	
 	// get profile types
 	$types = elgg_get_entities([
@@ -115,7 +131,7 @@ if ($admin_option) {
 		echo elgg_view_field([
 			'#type' => 'select',
 			'#label' => elgg_echo('profile_manager:profile:edit:custom_profile_type:label'),
-			'name' => 'custom_profile_fields[custom_profile_type]', 
+			'name' => 'custom_profile_fields[custom_profile_type]',
 			'options_values' => $options,
 		]);
 		
@@ -140,5 +156,9 @@ if ($admin_option) {
 	}
 }
 
-$footer = elgg_view('input/submit', ['value' => elgg_echo('register')]);
+$footer = elgg_view_field([
+	'#type' => 'submit',
+	'value' => elgg_echo('register'),
+	'#class' => 'elgg-foot',
+]);
 elgg_set_form_footer($footer);
