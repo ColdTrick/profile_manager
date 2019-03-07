76ac758232fa67446403763dcd10e6427118cb63
Jeroen Dalsem <jdalsem@coldtrick.com>
2019-03-07 12:39:07 +0100

    fixed: allow multiselect fields to be empty/removed

--- views/default/input/multiselect.php
+++ views/default/input/multiselect.php
@@ -22,5 +22,6 @@ if (!is_array($value)) {
 }
 $vars['value'] = $value;
 
+echo elgg_view('input/hidden', ['name' => $vars['name'], 'value' => '']);
 echo elgg_format_element('div', [], elgg_view('input/select', $vars));
 echo elgg_format_element('script', [], 'require(["jquery/multiselect", "profile_manager/multiselect"], function() { elgg.multiselect.init(); });');
