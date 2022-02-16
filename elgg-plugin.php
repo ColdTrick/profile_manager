0f5ffb8ca34ff9e76555708868c25b95497e3f79
Jerôme Bakker <jeabakker@coldtrick.com>
2022-02-16 15:30:24 +0100

    chore: wrapping up v17.0

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -11,7 +11,7 @@ require_once(dirname(__FILE__) . '/lib/functions.php');
 
 return [
 	'plugin' => [
-		'version' => '16.0.1',
+		'version' => '17.0',
 		'dependencies' => [
 			'profile' => ['position' => 'after'],
 		],
